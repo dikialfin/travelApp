e9f9eef1cd9ee44a80c13cb7f1dbce2ff009611b
dikialfin <muhammaddikialfin@outlook.com>
2022-09-26 13:38:42 +0700

    Course : 52 membuat fitur logout

--- lib/UI/Pages/settings_page.dart
+++ lib/UI/Pages/settings_page.dart
@@ -1,4 +1,8 @@
+import 'package:airplane/Shared/theme.dart';
+import 'package:airplane/UI/Widgets/customButton.dart';
+import 'package:airplane/cubit/auth_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({Key? key}) : super(key: key);
@@ -7,7 +11,35 @@ class SettingsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('Seetings Page'),
+        child: BlocConsumer<AuthCubit, AuthState>(
+          listener: (context, state) {
+            if (state is AuthInitial) {
+              Navigator.pushNamedAndRemoveUntil(
+                  context, '/get-started', (route) => false);
+            } else if (state is AuthFailed) {
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: Text(state.error),
+                backgroundColor: redColor,
+              ));
+            }
+          },
+          builder: (context, state) {
+            if (state is AuthLoading) {
+              return CircularProgressIndicator();
+            } else {
+              return CustomButton(
+                  textButton: Text(
+                    'Logout',
+                    style: whiteTextStyle,
+                  ),
+                  actionButton: () {
+                    context.read<AuthCubit>().signOut();
+                  },
+                  margin: EdgeInsets.all(0),
+                  width: 220);
+            }
+          },
+        ),
       ),
     );
   }
